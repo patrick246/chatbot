b67bbb3ee277904aa9837356a01f9a05981c4559
patrick246 <patricksilashahn@gmx.net>
2014-07-09 18:45:54 +0200

    initial setup

---
+++ bot.php
@@ -0,0 +1,4 @@
+<?php
+$auth_url = "http://chat.blackphantom.de/api/authentication.php";
+$loadMessage_url = "http://chat.blackphantom.de/api/loadLastMessages.php";
+$sendMessage_url = "http://chat.blackphantom.de/api/sendMessage.php";
